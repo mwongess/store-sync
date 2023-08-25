d67641778938400ce4f0071e6112a2a748079169
mwongess <amosmwongelah@gmail.com>
2023-08-25 14:13:49 +0300

    lazy load nested components

--- client/src/app/routes/app-routing.module.ts
+++ client/src/app/routes/app-routing.module.ts
@@ -4,17 +4,79 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'dashboard',
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'console',
+  },
+  {
+    path: 'console',
     loadComponent: () =>
       import('../pages/dashboard/dashboard.component').then(
         (dash) => dash.DashboardComponent
       ),
+    children: [
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('../pages/products/products.component').then(
+            (prdct) => prdct.ProductsComponent
+          ),
+      },
+      {
+        path: 'customers',
+        loadComponent: () =>
+          import('../pages/customers/customers.component').then(
+            (custms) => custms.CustomersComponent
+          ),
+      },
+      {
+        path: 'orders',
+        loadComponent: () =>
+          import('../pages/orders/orders.component').then(
+            (ords) => ords.OrdersComponent
+          ),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('../pages/settings/settings.component').then(
+            (stgs) => stgs.SettingsComponent
+          ),
+      },
+      {
+        path: 'notifications',
+        loadComponent: () =>
+          import('../pages/notifications/notifications.component').then(
+            (ntfs) => ntfs.NotificationsComponent
+          ),
+      },
+      {
+        path: 'reports',
+        loadComponent: () =>
+          import('../pages/reports/reports.component').then(
+            (rprts) => rprts.ReportsComponent
+          ),
+      },
+    ],
+  },
+// other routes
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('../pages/not-found/not-found.component').then(
+        (notfound) => notfound.NotFoundComponent
+      ),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'not-found',
   },
 ];
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
