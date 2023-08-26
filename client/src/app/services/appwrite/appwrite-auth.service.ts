4b4174a61be094d28d11728a47da4c93dd48e734
mwongess <amosmwongelah@gmail.com>
2023-08-26 12:50:19 +0300

    handle logic to upload images to cloudinary

--- client/src/app/services/appwrite/appwrite-auth.service.ts
+++ client/src/app/services/appwrite/appwrite-auth.service.ts
@@ -3,76 +3,79 @@ import { Client, Account, ID } from 'appwrite';
 import config from 'src/app/config/appwrite-config';
 
 type CreateUserAccount = {
-  email: string,
-  password: string,
-  name: string,
-}
+  email: string;
+  password: string;
+  name: string;
+};
 
 type LoginUserAccount = {
-  email: string,
-  password: string,
-}
+  email: string;
+  password: string;
+};
 
-const appwriteClient = new Client()
+const appwriteClient = new Client();
 
-appwriteClient.setEndpoint(config.appwriteUrl).setProject(config.appwriteProjectId);
+appwriteClient
+  .setEndpoint(config.appwriteUrl)
+  .setProject(config.appwriteProjectId);
 
-export const account = new Account(appwriteClient)
+export const account = new Account(appwriteClient);
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppwriteAuthService {
-
-  constructor() { }
+  constructor() {}
   async createUserAccount({ email, password, name }: CreateUserAccount) {
     try {
-        const userAccount = await account.create(ID.unique(), email, password, name)
-        if (userAccount) {
-            return this.login({ email, password })
-        } else {
-            return userAccount
-        }
+      const userAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
     } catch (error: any) {
-        throw error
+      throw error;
     }
+  }
 
-
-}
-
-async login({ email, password }: LoginUserAccount) {
+  async login({ email, password }: LoginUserAccount) {
     try {
-        return await account.createEmailSession(email, password)
+      return await account.createEmailSession(email, password);
     } catch (error: any) {
-        throw error
+      throw error;
     }
-}
+  }
 
-async isLoggedIn(): Promise<boolean> {
+  async isLoggedIn(): Promise<boolean> {
     try {
-        const data = await this.getCurrentUser();
-        return Boolean(data)
-    } catch (error) { }
+      const data = await this.getCurrentUser();
+      return Boolean(data);
+    } catch (error) {}
 
-    return false
-}
+    return false;
+  }
 
-async getCurrentUser() {
+  async getCurrentUser() {
     try {
-        return account.get()
+      return account.get();
     } catch (error) {
-        console.log("getcurrentUser error: " + error)
-
+      console.log('getcurrentUser error: ' + error);
     }
 
-    return null
-}
+    return null;
+  }
 
-async logout() {
+  async logout() {
     try {
-        return await account.deleteSession("current")
+      return await account.deleteSession('current');
     } catch (error) {
-        console.log("logout error: " + error)
+      console.log('logout error: ' + error);
     }
-}
+  }
 }
--- client/src/app/services/cloudinary/cloudinary.service.ts
+++ client/src/app/services/cloudinary/cloudinary.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CloudinaryService {
+  cloudName = "dd2zkoik3";
+  uploadPreset = 'aoh4fpwm';
 
-  constructor() { }
+  constructor() {}
+
+  createUploadWidget() {
+    cloudinary.createUploadWidget(
+        {
+          cloudName: this.cloudName,
+          uploadPreset: this.uploadPreset
+        },
+        (error:any, result:any) => {
+          if (!error && result && result.event === "success") {
+            console.log("Done uploading image! Here is the image info: ", result.info);
+
+            return result.info.secure_url //Uploaded image url!
+          }
+        }
+      );
+  }
 }
